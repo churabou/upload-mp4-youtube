5e971eeb78364f34bcd0172d60283484a624f0f0
chutatsu <>
2018-02-04 11:12:50 +0900

    create sample project

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by ちゅーたつ on 2018/02/04.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
